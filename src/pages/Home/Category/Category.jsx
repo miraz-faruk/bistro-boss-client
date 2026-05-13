ca678df7b7daf57698c68ce4982c2166607a37e8
mirazfaruk <mirazfaruk@gmail.com>
2026-05-13 22:29:00 +0600

    feat: implement category swiper with image mapping

---
+++ src/pages/Home/Category/Category.jsx
@@ -0,0 +1,45 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import slideImage1 from '../../../assets/home/slide1.jpg'
+import slideImage2 from '../../../assets/home/slide2.jpg'
+import slideImage3 from '../../../assets/home/slide3.jpg'
+import slideImage4 from '../../../assets/home/slide4.jpg'
+
+const Category = () => {
+
+    const categories = [
+        { name: 'Salad', image: slideImage1 },
+        { name: 'Pizza', image: slideImage2 },
+        { name: 'Soup', image: slideImage3 },
+        { name: 'Dessert', image: slideImage4 },
+    ]
+
+    return (
+        <Swiper
+            slidesPerView={4}
+            spaceBetween={30}
+            centeredSlides={true}
+            pagination={{
+                clickable: true,
+            }}
+            modules={[Pagination]}
+            className="mySwiper mb-16"
+        >
+
+            {
+                categories.map((item, index) =>
+                    <SwiperSlide key={index} className='relative pb-12'>
+                        <img src={item.image} alt={item.name} className='w-full object-cover' />
+                        <h3 className='absolute -mt-20 left-0 right-0 text-2xl text-center text-white font-bold uppercase drop-shadow-2xl'>{item.name}</h3>
+                    </SwiperSlide>
+                )
+            }
+        </Swiper>
+    );
+};
+
+export default Category;
