675c184d2b0fc3677f1bbc59d2b25ebbd1eb53ae
mirazfaruk <mirazfaruk@gmail.com>
2026-05-13 21:22:22 +0600

    feat: implement banner with full screen carousel

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -8,23 +8,28 @@ import img4 from '../../../assets/home/04.jpg';
 import img5 from '../../../assets/home/05.png';
 
 const Banner = () => {
+
+    const images = [img1, img2, img3, img4, img5];
+
     return (
-        <Carousel className='max-h-screen'>
-            <div>
-                <img src={img1} />
-            </div>
-            <div>
-                <img src={img2} />
-            </div>
-            <div>
-                <img src={img3} />
-            </div>
-            <div>
-                <img src={img4} />
-            </div>
-            <div>
-                <img src={img5} />
-            </div>
+        <Carousel
+            autoPlay
+            infiniteLoop
+            showStatus={false}
+            showThumbs={true}
+            bottom thumbnails
+            className="text-center"
+        >
+            {images.map((img, index) => (
+                <div key={index} className="relative h-[80vh] md:h-screen">
+                    {/* The Background Image */}
+                    <img
+                        src={img}
+                        className="h-full w-full object-cover"
+                        alt="banner"
+                    />
+                </div>
+            ))}
         </Carousel>
     );
 };
