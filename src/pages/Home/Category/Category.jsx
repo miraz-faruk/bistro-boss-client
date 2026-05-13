8741fc7d1cf87da85f6e06a91f771b3fe4cfa7b1
mirazfaruk <mirazfaruk@gmail.com>
2026-05-14 01:15:33 +0600

    feat: create reusable SectionTitle component for dynamic headings

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -8,6 +8,9 @@ import slideImage1 from '../../../assets/home/slide1.jpg'
 import slideImage2 from '../../../assets/home/slide2.jpg'
 import slideImage3 from '../../../assets/home/slide3.jpg'
 import slideImage4 from '../../../assets/home/slide4.jpg'
+import slideImage5 from '../../../assets/home/slide5.jpg'
+
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 
 const Category = () => {
 
@@ -16,29 +19,39 @@ const Category = () => {
         { name: 'Pizza', image: slideImage2 },
         { name: 'Soup', image: slideImage3 },
         { name: 'Dessert', image: slideImage4 },
+        { name: 'Kebab', image: slideImage5 },
     ]
 
     return (
-        <Swiper
-            slidesPerView={4}
-            spaceBetween={30}
-            centeredSlides={true}
-            pagination={{
-                clickable: true,
-            }}
-            modules={[Pagination]}
-            className="mySwiper mb-16"
-        >
+        <section>
+            <SectionTitle
+                subHeading={'---From 11"00am to 10:00pm---'}
+                heading={'ORDER ONLINE'}
+            ></SectionTitle>
 
-            {
-                categories.map((item, index) =>
-                    <SwiperSlide key={index} className='relative pb-12'>
-                        <img src={item.image} alt={item.name} className='w-full object-cover' />
-                        <h3 className='absolute -mt-20 left-0 right-0 text-2xl text-center text-white font-bold uppercase drop-shadow-2xl'>{item.name}</h3>
-                    </SwiperSlide>
-                )
-            }
-        </Swiper>
+            <Swiper
+                slidesPerView={4}
+                spaceBetween={30}
+                centeredSlides={true}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper mb-16"
+            >
+
+                {
+                    categories.map((item, index) =>
+                        <SwiperSlide key={index} className='relative pb-12'>
+                            <div className='h-[500px] w-full overflow-hidden rounded-lg relative'>
+                                <img src={item.image} alt={item.name} className='w-full h-full object-cover' />
+                            </div>
+                            <h3 className='absolute -mt-20 left-0 right-0 text-2xl text-center text-white font-bold uppercase drop-shadow-2xl'>{item.name}</h3>
+                        </SwiperSlide>
+                    )
+                }
+            </Swiper>
+        </section>
     );
 };
 
