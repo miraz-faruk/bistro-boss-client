6cfa4456f1d69a5c9e616a88011552412b58b6c7
mirazfaruk <mirazfaruk@gmail.com>
2026-06-10 21:46:41 +0600

    feat: create custom useMenu hook for load data globally

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,13 +1,8 @@
-// import { Helmet } from "react-helmet-async";
 import PageTitle from "../../../components/PageTitle/PageTitle";
-
 
 const Menu = () => {
     return (
         <div>
-            {/* <Helmet>
-                <title>Menu</title>
-            </Helmet> */}
             <PageTitle title="Menu" />
         </div>
     );
