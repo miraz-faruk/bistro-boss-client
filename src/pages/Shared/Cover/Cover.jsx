a146230b3c80f1dd023bd221154a471e0ed9e15e
mirazfaruk <mirazfaruk@gmail.com>
2026-06-12 19:38:56 +0600

    feat: add paralax effect to the cover component

--- src/pages/Shared/Cover/Cover.jsx
+++ src/pages/Shared/Cover/Cover.jsx
@@ -1,23 +1,27 @@
-
+import { Parallax } from 'react-parallax';
 
 const Cover = ({ img, title, desc }) => {
     return (
-        <div
-            className="hero h-[700px] bg-fixed"
-            style={{
-                backgroundImage:
-                    `url("${img}")`,
-            }}
+        <Parallax
+            blur={{ min: -15, max: 15 }}
+            bgImage={img}
+            bgImageAlt="the dog"
+            strength={-200}
         >
-            <div className="hero-overlay"></div>
-            <div className="hero-content text-white text-center">
-                <div className="max-w-4xl bg-[#151501] bg-opacity-50 p-10">
-                    <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
-                    <p className="mb-5">{desc}
-                    </p>
+            <div
+                className="hero h-[700px] bg-fixed"
+            >
+                <div className="hero-overlay"></div>
+                <div className="hero-content text-white text-center">
+                    <div className="max-w-4xl bg-[#151501] bg-opacity-50 p-10">
+                        <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
+                        <p className="mb-5">{desc}
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Parallax>
+
     );
 };
 
